ff76b8eca51ea32d56a51667038c753c1d859077
mishesbone <89227538+mishesbone@users.noreply.github.com>
2026-03-20 12:15:25 +0000

    Refactor ASGI configuration to include Channels support

--- config/asgi.py
+++ config/asgi.py
@@ -1,13 +1,18 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 import apps.collaboration.routing
 
+# Set the Django settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+# ASGI application
 application = ProtocolTypeRouter({
+    # HTTP requests go to standard Django ASGI app
     "http": get_asgi_application(),
+
+    # WebSocket requests go through Channels AuthMiddleware and URLRouter
     "websocket": AuthMiddlewareStack(
         URLRouter(
             apps.collaboration.routing.websocket_urlpatterns
